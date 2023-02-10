95f4c7715ab6adfedad5dd150879c5826e4ceb55
Andrew <rassandev@gmail.com>
2023-02-10 14:51:19 +0300

    add Parser.java

---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Parser {
+    public static Map<String, Object> parse(String dataForParsing) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> result = mapper.readValue(dataForParsing, new TypeReference<>(){});
+        return result;
+    }
+}
