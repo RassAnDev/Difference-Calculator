eee5ab4c67f6f1f352b4c5cbdcac959409e93ffd
Andrew <rassandev@gmail.com>
2023-02-14 15:04:25 +0300

    fix TestStylish.java

--- app/src/test/java/TestStylish.java
+++ app/src/test/java/TestStylish.java
@@ -9,13 +9,19 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestStylish {
+    static String file1;
+    static String file2;
     static String expected;
     static String actual;
     @BeforeAll
     public static void initData() throws Exception {
+        Path directoryToFile1 = Paths.get("src/test/resources/file1.json").toAbsolutePath().normalize();
+        Path directoryToFile2 = Paths.get("src/test/resources/file2.json").toAbsolutePath().normalize();
         Path directoryToResult = Paths.get("src/test/resources/result_stylish.txt").toAbsolutePath().normalize();
+        file1 = directoryToFile1.toString();
+        file2 = directoryToFile2.toString();
         expected = Files.readString(directoryToResult);
-        actual = Differ.generate("filepath1.json", "filepath2.json");
+        actual = Differ.generate(file1, file2);
     }
 
     @Test
