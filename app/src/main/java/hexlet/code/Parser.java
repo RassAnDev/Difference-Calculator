1e6e5960278c55c970cd844d7204d599614613c7
Andrew <rassandev@gmail.com>
2023-02-17 17:03:17 +0300

    add a new method for parsing yaml files

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,14 +2,19 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parse(String dataForParsing) throws IOException {
+    public static Map<String, Object> parseJson(String dataForParsing) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> result = mapper.readValue(dataForParsing, new TypeReference<>() { });
-        return result;
+        return mapper.readValue(dataForParsing, new TypeReference<>() { });
+    }
+
+    public static Map<String, Object> parseYaml(String dataForParsing) throws IOException {
+        ObjectMapper mapper = new YAMLMapper();
+        return mapper.readValue(dataForParsing, new TypeReference<>() { });
     }
 }
