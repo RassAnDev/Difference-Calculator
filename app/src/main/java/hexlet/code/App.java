516b59cebfeb13480a7963128ecbb14d256ad5af
Andrew <rassandev@gmail.com>
2023-02-21 12:52:54 +0300

    set stylish as default output format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,7 +10,8 @@ import java.util.concurrent.Callable;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
-    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
+            description = "output format [default: stylish]")
     private String formatName;
 
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
