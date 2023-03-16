8ae93a1f59a0f6800eecaedfd0eccd7cba91eadd
Andrew <rassandev@gmail.com>
2023-03-16 19:52:25 +0300

    fix JsonFormatter.java

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -8,11 +8,6 @@ import java.util.Map;
 public class JsonFormatter {
     public static String format(List<Map<String, Object>> listForFormatting) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        StringBuilder result = new StringBuilder();
-
-        for (Map<String, Object> elem : listForFormatting) {
-            result.append(mapper.writeValueAsString(elem)).append(",\n");
-        }
-        return result.deleteCharAt(result.length() - 2).toString();
+        return mapper.writeValueAsString(listForFormatting);
     }
 }
