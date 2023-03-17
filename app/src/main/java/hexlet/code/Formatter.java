c749b0fda5120a7a81669f6bb073cc1179380f0e
Andrew <rassandev@gmail.com>
2023-03-17 20:23:19 +0300

    fix Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -9,14 +9,24 @@ import java.util.Map;
 
 public class Formatter {
     public static String format(List<Map<String, Object>> listForFormatting, String requiredFormat) throws Exception {
-        if (requiredFormat.equals("stylish")) {
-            return StylishFormatter.format(listForFormatting);
-        } else if (requiredFormat.equals("plain")) {
-            return PlainFormatter.format(listForFormatting);
-        } else if (requiredFormat.equals("json")) {
-            return JsonFormatter.format(listForFormatting);
-        } else {
-            throw new IllegalArgumentException();
+        switch (requiredFormat) {
+            case "stylish":
+                return StylishFormatter.format(listForFormatting);
+            case "plain":
+                return PlainFormatter.format(listForFormatting);
+            case "json":
+                return JsonFormatter.format(listForFormatting);
+            default:
+                throw new IllegalArgumentException();
         }
+//        if (requiredFormat.equals("stylish")) {
+//            return StylishFormatter.format(listForFormatting);
+//        } else if (requiredFormat.equals("plain")) {
+//            return PlainFormatter.format(listForFormatting);
+//        } else if (requiredFormat.equals("json")) {
+//            return JsonFormatter.format(listForFormatting);
+//        } else {
+//            throw new IllegalArgumentException();
+//        }
     }
 }
